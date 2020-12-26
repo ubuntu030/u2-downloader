cb00709fd0551f29bea0a444e3225fafcb09720d
ubuntu030 <ubuntu030@gmail.com>
2020-12-26 19:35:32 +0800

    處理跨域請求 暫時將檔案存進./video

--- server/server.js
+++ server/server.js
@@ -3,15 +3,26 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const ytdl = require('ytdl-core');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+const PORT = 8080;
+// 處理跨域請求
+app.use(cors());
+// app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.json());
 
 app.post('/', function (req, res) {
-	ytdl.getInfo('https://youtu.be/NO8iSOv3BBU')
+	// TODO: url vertify
+	ytdl.getInfo(req.body.url)
 		.then(info => {
 			if (typeof info === 'object' && info.videoDetails && info.videoDetails.title) {
 				resObj = { status: 'success' };
 				// 取回資料後進行下載
 				let stream = ytdl.downloadFromInfo(info);
-				let writable = fs.createWriteStream(path.join(__dirname, '../public', info.videoDetails.title + '.mp4'));
+				// 在 react-scripts start 啟動下若將檔案寫入public下會觸發refresh
+				// let writable = fs.createWriteStream(path.join(__dirname, '../public', info.videoDetails.title + '.mp4'));
+				let writable = fs.createWriteStream(path.join(__dirname, '../video', info.videoDetails.title + '.mp4'));
 				stream.pipe(writable);
 				stream.on('finish', function () {
 					res.json(resObj); //write a response to the client
@@ -33,6 +44,6 @@ app.get('/', (req, res) => {
 	res.json({ status: 'fail' })
 })
 
-app.listen(8080, () => {
-	console.log('Example app listening at http://localhost:8080')
+app.listen(PORT, () => {
+	console.log(`Example app listening at http://localhost:${PORT}`)
 })
