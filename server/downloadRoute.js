63fa679e3f1d077cbf80d411a209ca4dd6f07bc4
ubuntu030 <ubuntu030@gmail.com>
2020-12-29 19:36:34 +0800

    add error handle

--- server/downloadRoute.js
+++ server/downloadRoute.js
@@ -18,58 +18,69 @@ router.use(function (req, res, next) {
 });
 
 router.post('/', function (req, res) {
-	// TODO: url vertify 
-	ytdl.getInfo(req.body.url)
-		.then(info => {
-			if (typeof info === 'object' && info.videoDetails && info.videoDetails.title) {
-				const VIDEO_NAME = info.videoDetails.title;
-				const SAVE_FILE_PATH = path.join(PATH_VIDEO, VIDEO_NAME + '.mp4');
-				let resObj = {
-					status: 'fail',
-					errmsg: '',
-					file: { name: '', path: '', id: '' }
-				};
-				/**
-				 * 無法下載擁有最高品質視訊&音訊 https://github.com/fent/node-ytdl-core/issues/374
-				 * 若要最高品質視音訊，需個下載並合併
-				 */
-				// 取回資料後進行下載
-				let stream = ytdl.downloadFromInfo(
-					info,
-					{ quality: 'highest' }
-				);
-				// 在 react-scripts start 啟動下若將檔案寫入public下會觸發refresh
-				// let writable = fs.createWriteStream(path.join(__dirname, '../public', info.videoDetails.title + '.mp4'));
-				let writable = fs.createWriteStream(SAVE_FILE_PATH);
-				stream.pipe(writable);
-				stream.on('finish', function () {
-					// Use fs.readFile() method to read the file 
-					fs.readFile(SAVE_FILE_PATH, 'utf8', function (err, data) {
-						if (err) {
-							resObj.errmsg = err;
-							res.json(resObj); //write a response to the client
-						}
-						// 確認可讀到file則回傳檔案路徑與檔案名
-						resObj.file = {
-							name: VIDEO_NAME,
-							path: SAVE_FILE_PATH,
-							id: info.videoDetails.videoId,
-							mp3path: ''
-						}
+	try {
+		// TODO: url vertify 
+		ytdl.getInfo(req.body.url)
+			.then(info => {
+				if (typeof info === 'object' && info.videoDetails && info.videoDetails.title) {
+					const VIDEO_NAME = info.videoDetails.title;
+					const SAVE_FILE_PATH = path.join(PATH_VIDEO, VIDEO_NAME + '.mp4');
+					let resObj = {
+						status: 'fail',
+						errmsg: '',
+						file: { name: '', path: '', id: '' }
+					};
+					/**
+					 * 無法下載擁有最高品質視訊&音訊 https://github.com/fent/node-ytdl-core/issues/374
+					 * 若要最高品質視音訊，需個下載並合併
+					 */
+					// 取回資料後進行下載
+					let stream = ytdl.downloadFromInfo(
+						info,
+						{ quality: 'highest' }
+					);
+					// 在 react-scripts start 啟動下若將檔案寫入public下會觸發refresh
+					// let writable = fs.createWriteStream(path.join(__dirname, '../public', info.videoDetails.title + '.mp4'));
+					let writable = fs.createWriteStream(SAVE_FILE_PATH);
+					stream.pipe(writable);
+					stream.on('finish', function () {
+						// Use fs.readFile() method to read the file 
+						fs.readFile(SAVE_FILE_PATH, 'utf8', function (err, data) {
+							if (err) {
+								resObj.errmsg = err;
+								res.json(resObj); //write a response to the client
+							}
+							// 確認可讀到file則回傳檔案路徑與檔案名
+							resObj.file = {
+								name: VIDEO_NAME,
+								path: SAVE_FILE_PATH,
+								id: info.videoDetails.videoId,
+								mp3path: ''
+							}
+							res.json(resObj);
+						});
+					});
+					stream.on('error', function (err) {
+						resObj = { status: 'fail', errmsg: err };
+						console.error(err);
 						res.json(resObj);
 					});
-				});
-				stream.on('error', function (err) {
-					resObj = { status: 'fail', errmsg: err };
-					console.log(err);
+				} else {
 					res.json(resObj);
-				});
-			} else {
-				res.json(resObj);
-			}
-		}).catch(error => {
-			res.json(error)
+				}
+			}).catch(error => {
+				res.json({
+					status: 'fail',
+					errmsg: error.message,
+				})
+			});
+	} catch (err) {
+		console.error(err);
+		res.send({
+			status: 'fail',
+			errmsg: err.message,
 		});
+	}
 })
 
 
